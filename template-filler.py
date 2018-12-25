8cbc76da0c3bc176c8fdbdd920d1d1fa0c9ced6e
Dan Poling <dan.poling@gmail.com>
2018-12-25 12:42:10 -0500

    Update template-filler.py

--- template-filler.py
+++ template-filler.py
@@ -1,18 +1,3 @@
-'''
-This program uses a excel sheet to fill in a word doc template to 
-make a new document.
-
-template + sheet = result
-
-The docx template needs unique placeholders that match up with the 
-first column in the xlsx sheet. The second column of the xlsx sheet
-should contain the values that will flow into the final form.
-
-This program uses the following libraries:
-    - re to find/replace
-    - python-docx to read & save .docx
-    - pandas to read in .xlsx
-'''
 
 def template_filler(template_filename, sheet_filename, result_filename):
     import re
@@ -52,8 +37,3 @@ def template_filler(template_filename, sheet_filename, result_filename):
 
     # Save our results as a new Word Document
     doc.save(result_filename)
-
-
-#%%
-template_filler('template_will.docx', 'will_data-Andrew.xlsx', 'result1.docx')
-
